e98152f560d2e727b53ee4a3e4e35566bcb88d64
Daniel Eros <drerosdaniel@gmail.com>
2017-11-20 10:16:21 +0100

    fff

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        print >>sys.stderr, "fatal error"
+        print >>sys.stderr, "Aceplusplus error"
         return 6
 
     def showdown(self, game_state):
