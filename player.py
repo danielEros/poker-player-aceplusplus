341b97635865b804d3392703f69197366b2151ef
Daniel Eros <drerosdaniel@gmail.com>
2017-11-20 10:48:02 +0100

    print player

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        sys.stderr.write("ERRROR WRITE ")
+        sys.stderr.write(game_state["players"])
         return game_state["current_buy_in"]
 
     def showdown(self, game_state):
