67678d3adef98cae0d6528bad63ec6496af22a59
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-11-20 14:15:06 +0100

    pair count

--- player.py
+++ player.py
@@ -3,6 +3,12 @@ import sys
 
 class Player:
     VERSION = "Default Python folding player"
+    
+    def has_pair(card_list):
+        count = [i for i in card_list if card_list.count(i) > 1]
+        print len(count)
+        if len(count) > 1:
+            return true
 
     def betRequest(self, game_state):
         ourID = game_state["in_action"]
@@ -11,6 +17,18 @@ class Player:
         #print card_1_rank
         #print card_2_rank
         high_rank = ["8", "9", "10", "J", "Q", "K", "A"]
+        card_list = []
+        card_list.append(card_1_rank)
+        card_list.append(card_2_rank)
+        if "community_cards" in game_state:
+            for i in game_state["community_cards"]:
+                card_list.append(i["rank"])
+                #print i["rank"]
+        #has_pair(card_list)
+        count = [i for i in card_list if card_list.count(i) > 1]
+        #print count
+        if len(count) >= 2: 
+            return game_state["current_buy_in"]
         if card_1_rank == card_2_rank:
             #print "pair"
             return game_state["current_buy_in"]
