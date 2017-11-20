6e0e172986fe18e38c3a6e9ccf9a7b6048d733a1
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-11-20 14:24:28 +0100

    hold

--- player.py
+++ player.py
@@ -17,6 +17,7 @@ class Player:
         #print card_1_rank
         #print card_2_rank
         high_rank = ["8", "9", "10", "J", "Q", "K", "A"]
+        #highest_rank = ["J", "Q", "K", "A"]
         card_list = []
         card_list.append(card_1_rank)
         card_list.append(card_2_rank)
@@ -27,6 +28,7 @@ class Player:
         #has_pair(card_list)
         count = [i for i in card_list if card_list.count(i) > 1]
         #print count
+        #if len(count) >= 2 and (card_1_rank in highest_rank or card_2_rank in highest_rank or 
         if len(count) >= 2: 
             return game_state["current_buy_in"]
         if card_1_rank == card_2_rank:
@@ -34,7 +36,7 @@ class Player:
             return game_state["current_buy_in"]
         if card_1_rank in high_rank or card_2_rank in high_rank: 
             #print "high"
-            return game_state["current_buy_in"]
+            return game_state["current_buy_in"] - players["in_action"]["bet"]
         else:
             return 0
 
