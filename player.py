98d7ea99f4dfd9bc0baad9ae0f79cccec208898d
Daniel Eros <drerosdaniel@gmail.com>
2017-11-20 10:35:07 +0100

    log try

--- player.py
+++ player.py
@@ -1,8 +1,12 @@
+import sys
+
 
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        print "ITTT   TT"
+        sys.stderr.write("ERRROR WRITE ")
         return game_state["current_buy_in"]
 
     def showdown(self, game_state):
