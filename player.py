8d9731c2414802de495876021ee3c8736dbab13e
Daniel Eros <drerosdaniel@gmail.com>
2017-11-20 10:13:44 +0100

    print test

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        eprint "Testin Acepp"
         return 6
 
     def showdown(self, game_state):
