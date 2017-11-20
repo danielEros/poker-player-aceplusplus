14e797cdc9661de8319b3c131b2a80a9789842c9
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-11-20 13:52:08 +0100

    pair, high

--- player.py
+++ player.py
@@ -5,8 +5,20 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        #sys.stderr.write(game_state["players"])
-        return game_state["current_buy_in"]
+        ourID = game_state["in_action"]
+        card_1_rank = game_state["players"][ourID]["hole_cards"][0]["rank"]
+        card_2_rank = game_state["players"][ourID]["hole_cards"][1]["rank"]
+        #print card_1_rank
+        #print card_2_rank
+        high_rank = ["8", "9", "10", "J", "Q", "K", "A"]
+        if card_1_rank == card_2_rank:
+            #print "pair"
+            return game_state["current_buy_in"]
+        if card_1_rank in high_rank or card_2_rank in high_rank: 
+            #print "high"
+            return game_state["current_buy_in"]
+        else:
+            return 0
 
     def showdown(self, game_state):
         pass
