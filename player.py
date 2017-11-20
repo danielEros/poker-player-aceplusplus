8bcee539d2e69dae100920a7af4993531708ffbe
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-11-20 14:53:30 +0100

    fix bug players

--- player.py
+++ player.py
@@ -17,6 +17,7 @@ class Player:
         #print card_1_rank
         #print card_2_rank
         high_rank = ["10", "J", "Q", "K", "A"]
+        keepable_pair = ["7", "8", "9", "10", "J", "Q", "K", "A"]
         #highest_rank = ["J", "Q", "K", "A"]
         card_list = []
         card_list.append(card_1_rank)
@@ -29,19 +30,19 @@ class Player:
         count = [i for i in card_list if card_list.count(i) > 1]
         #print count
         #if len(count) >= 2 and (card_1_rank in highest_rank or card_2_rank in highest_rank or 
-        if len(count) >= 2: 
+        if len(count) >= 2 and count[0] in keepable_pair: 
             return game_state["current_buy_in"]
-        if card_1_rank == card_2_rank:
-            #print "pair"
-            return game_state["current_buy_in"]
+        #if card_1_rank == card_2_rank and card_1_rank in keepable_pair:
+         #   #print "pair"
+          #  return game_state["current_buy_in"]
         if card_1_rank in high_rank or card_2_rank in high_rank: 
             #print "high"
-            return game_state["current_buy_in"] - players["in_action"]["bet"]
+            return game_state["current_buy_in"] - game_state["players"]["in_action"]["bet"]
         else:
-            current_bet = game_state["current_buy_in"] - players["in_action"]["bet"]
+            current_bet = game_state["current_buy_in"] - game_state["players"]["in_action"]["bet"] 
             stack = game_state["players"][ourID]["stack"]
             if current_bet < stack/3:
-                return game_state["current_buy_in"] - players["in_action"]["bet"]
+                return game_state["current_buy_in"] - game_state["players"]["in_action"]["bet"]
             return 0
 
     def showdown(self, game_state):
